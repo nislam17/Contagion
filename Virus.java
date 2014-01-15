0aa3834129133196c13234b5762d45bd60e6326f
nislam17 <nislam17@gmail.com>
2014-01-14 23:45:27 -0500

    Update Virus.java
    Added all methods and vars from super class Pathogen

--- Virus.java
+++ Virus.java
@@ -1,4 +1,32 @@
-
 public class Virus extends Pathogen {
-
+	
+	public Virus() {
+		infectivity = 0; 
+		resistivity = 0; 
+		lethality = 0; 
+	}
+	
+	public void transmit() {
+		
+	}
+	
+	public void infect() {
+		
+	}
+	
+	public void resist() {
+		
+	}
+	
+	public void upgradeInfectivity() {
+		
+	}
+	
+	public void upgradeResistivity() {
+		
+	}
+	
+	public void upgradeLethality() {
+		
+	}
 }
