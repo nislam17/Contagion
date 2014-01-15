0f4c10b6565c5af9f4d3bd3c8df0a5d29448e696
nislam17 <nislam17@gmail.com>
2014-01-14 23:46:04 -0500

    Update Parasite.java
    Added all methods and vars from super class Pathogen

--- Parasite.java
+++ Parasite.java
@@ -1,4 +1,32 @@
-
 public class Parasite extends Pathogen {
-
+	
+	public Parasite() {
+		infectivity = 0; 
+		resistivity = 0; 
+		lethality = 0; 
+	}
+	
+	public void transmit() {
+		
+	}
+	
+	public void infect() {
+		
+	}
+	
+	public void resist() {
+		
+	}
+	
+	public void upgradeInfectivity() {
+		
+	}
+	
+	public void upgradeResistivity() {
+		
+	}
+	
+	public void upgradeLethality() {
+		
+	}
 }
