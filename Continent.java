00fe7e1ca03c5f9e0d9cd570b9357eebfbd33800
nislam17 <nislam17@gmail.com>
2014-01-24 00:50:42 -0500

    Update Continent.java

--- Continent.java
+++ Continent.java
@@ -1,6 +1,7 @@
 public class Continent {
-	private int _population, _infected = 0, _dead = 0, _cure = 0; 
-	private double _chances,
+	private int _population, _oldInfected = 0, _infected = 0, _dead = 0, _cure = 0; 
+	private double _chances;
+	private boolean _invaded = false;
 	
 	
 	public Continent(int p, double c) {
@@ -13,29 +14,48 @@ public class Continent {
 		return _infected;
 	}
 	
+	public int getOldInfected() {
+		return _oldInfected;
+	}
+	
 	public int getDead(){
 		return _dead;
 	}
 	
-	public double getcure(){
+	public double getCure(){
 		return _cure;
 	}
 	
+	public boolean getInvaded() {
+		return _invaded; 
+	}
+	
 	// ============ Mutator Methods ==================
-	public setInfected(int i){
+	public void setOldInfected(int i) {
+		_oldInfected += i; 
+		
+	}
+	public int setInfected(int i){
+		int oldInfected = _infected;
 		_infected += i;
+		return oldInfected;
 	}
 	
-	public setDead(int d){
-		_dead += d
+	public int setDead(int d){
+		int oldDead = _dead;
+		_dead += d;
+		return oldDead; 
 	}
 	
-	public setcure(int f){
+	public int setCure(double f){
+		int oldCure = _cure;
 		_cure += f;
+		return oldCure;
 	}
 	
-	
-	
+	public void setInvaded(boolean invaded) {
+		_invaded = invaded;
+	}
 	
 	
 }
