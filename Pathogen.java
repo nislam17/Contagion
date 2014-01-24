467ea6a161e8124fb8f09bd4c9eab978bbab06ba
nislam17 <nislam17@gmail.com>
2014-01-24 02:31:09 -0500

    Update Pathogen.java

--- Pathogen.java
+++ Pathogen.java
@@ -7,14 +7,7 @@ public abstract class Pathogen {
 	//lethality - How easily the disease can kill someone! It can slow/stop cure research and give you more DNA points.
 	
 	private int _DNApoints; 
-	// DNA points let you evolve. Get them automatically
-	
-	
-	public Pathogen() {
-		_infectivity = 0; 
-		_resistivity = 0; 
-		_lethality = 0; 
-	} 
+	// DNA points let you evolve. Get them automatically.
 	
 	//===========================ACTIONS================================
 	
@@ -22,31 +15,27 @@ public abstract class Pathogen {
 		_DNApoints += p;
 	}
 	
-	/*Pre-cond: 
-	 * Post-cond: 
-	 * transmit works based on infectivity and controls how fast the disease spreads inside countries*/
+	/* transmit works based on infectivity and controls how fast the disease spreads inside countries*/
 	public  void transmit(Continent c) {
-		c.setInvaded(true);
-		increaseDNApoints(10);
+		c.setInvaded(true); //signifies when the pathogen first enters a continent
+		increaseDNApoints(10); //first time a pathogen enters a continent, player receives lotsa points
 	}
 	
-	/*Pre-cond: 
-	 * Post-cond: 
-	 * infect works based on infectivity and controls how fast the disease spreads between countries*/
+	/* infect works based on infectivity and controls how fast the disease spreads between countries*/
 	public void infect(Continent c) {
-		if (Math.random() < _infectivity) {
+		if (Math.random() < _infectivity) { 
 			if (! c.getInvaded()) { //If it's the first time the continent is being invaded, infect 1 person
 				increaseDNApoints(c.setInfected(1));
 				
 			}
 			
 			else if (c.getPopulation() == c.getInfected()) {
-				return;
+				return; // if the entire continent has been infected, stop infecting
 			}
 			else {
 				int points = c.getOldInfected() + c.getInfected();
 				increaseDNApoints(points);
-				c.setOldInfected(c.setInfected(points));
+				c.setOldInfected(c.setInfected(points)); //based on the fib code, infect peeps
 			}
 			
 			
@@ -58,58 +47,54 @@ public abstract class Pathogen {
 	 * resist works based on resistivity and slows down the cure */
 	public void resist(Continent c) {
 		if (Math.random() < _resistivity) {
-			c.setCure((c.getCure() - 1));
+			if (c.getCure() >= 1) {
+				c.setCure((c.getCure() - 1)); /* if the production of the cure has been started, pathogen
+				 								* will start to resist */
+												
+			}
 		}
 	}
 	
-	/*Pre-cond: 
-	 * Post-cond: 
-	 * DNApoints increase based on how many people are infected + when pathogen is transmitted to a new country  */
-	 
+	
 	 	
-	/*Pre-cond: 
-	 * Post-cond: 
-	 * kill works based on lethality and controls how fast the disease wipes out the population*/
+	/* kill works based on lethality and controls how fast the disease wipes out the population*/
 	public void kill(Continent c) {
 		if (Math.random() < _lethality) {
+			if (!c.getInvaded()) {
+				return; // if the country hasn't been infected yet, don't kill anyone
+			}
 			if (c.getDead() == 0) {
-				c.setDead(1);
+				c.setDead(1); // if no one in the country is dead yet, start by killing 1 person
 			}
 			
 			else if (c.getPopulation() == c.getDead()) {
-				c.setDeadContinent(true);
-				return;
+				c.setDeadContinent(true); 
+				return; // if everyone has been wiped out, don't do anything
 			}
 			else {
 				int points = c.getOldDead() + c.getDead(); 
 				increaseDNApoints(points);
 				c.setOldDead(c.setDead(points));
 			}
-			c.setDead(c.getDead() * c.getDead()); 
+			c.setDead(c.getDead() * c.getDead());  // kill ppl based on the fib code
 		}
 	}
 	
 	
-	//=====================UPGRADES======================
-	/*Pre-cond: 
-	 * Post-cond: 
-	 * DNA points help upgrade Transmission */
+	//=====================UPGRADES====================== ummm gonna need help with this part 
+	/*DNA points help upgrade Transmission */
 	public void upgradeTransmission() {
 		_DNApoints -= 9; 
 		_infectivity += .1; 
 	} // upgrades infectivity
 	
-	/*Pre-cond: 
-	 * Post-cond: 
-	 * DNA points help upgrade symptoms */
+	/* DNA points help upgrade symptoms */
 	public void upgradeSymptoms() {
 		_DNApoints -= 9; 
 		_resistivity += .1; 
 	}
 	
-	/*Pre-cond: 
-	 * Post-cond: 
-	 * DNA points help upgrade abilities */
+	/*DNA points help upgrade abilities */
 	public void upgradeAbilities() {
 		_DNApoints -= 11; 
 		_lethality += .05; 
