3271e3fa2b0866d0d9bdef9a808d9c568704386f
Ruzeb <ruzeb1996@gmail.com>
2014-01-24 03:48:50 -0500

    Update Upgrades.java

--- Upgrades.java
+++ Upgrades.java
@@ -3,34 +3,55 @@ import javax.swing.*;
 import java.net.*;
  
 public class Upgrades {
- 
-    public Upgrades() throws MalformedURLException{
+	
+	
+
+    public Upgrades(JButton a, JButton b, JButton c, 
+    		JButton d, JButton e, JButton f, JButton g, JButton h) {
+    	JPanel p = new JPanel();
+    	
+    	p.add(a);
+    	p.add(b);
+    	p.add(c);
+    	p.add(d);
+    	p.add(e);
+    	p.add(f);
+    	p.add(g);
+    	p.add(h);
+    	 p.setBorder(BorderFactory.createEmptyBorder(100, 100, 700, 100));  
+    	
+    	
+        JFrame f1 = new JFrame("Upgrades");
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+        f1.setContentPane(p);
+        f1.pack();
+        f1.setVisible(true);
+        buttoner(a);
+        buttoner(b);
+        buttoner(c);
+        buttoner(d);
+        buttoner(e);
+        buttoner(f);
+        buttoner(g);
+        buttoner(h);
+    }
     
+    public void buttoner(JButton b){
     	ImageIcon red = new ImageIcon("red.png");
     	ImageIcon gray = new ImageIcon("gray.png");
     
-        JButton b = new JButton("Round");
+        
         b.setIcon(red);
         b.setRolloverIcon(gray);
+        
         
         b.setHorizontalTextPosition(JButton.CENTER);
         b.setRolloverEnabled(true);
         b.setFocusPainted(false);
         b.setBorderPainted(false);
         b.setContentAreaFilled(false);
- 
-    JFrame f = new JFrame("round button test");
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    JPanel p = new JPanel();
-        p.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
-        //p.add(b0);
-        p.add(b);
-        f.setContentPane(p);
-        f.pack();
-        f.setVisible(true);
     }
     
-    public static void main(String[] args) throws MalformedURLException{
-    	Upgrades u = new Upgrades();
-    }
+
 }
