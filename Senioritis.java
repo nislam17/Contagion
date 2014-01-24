5d70d15e13937bc312e6538c7526a06698762bbd
nislam17 <nislam17@gmail.com>
2014-01-24 01:21:43 -0500

    Update and rename Parasite.java to Senioritis.java

---
+++ Senioritis.java
@@ -0,0 +1,9 @@
+public class Senioritis extends Pathogen {
+	
+	public Senioritis (double infectivity, double resistivity, double lethality) {
+		
+		_infectivity = infectivity; 
+		_resistivity = resistivity; 
+		_lethality = lethality; 
+	} 
+}
