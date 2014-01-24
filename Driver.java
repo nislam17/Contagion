5c88de6e451fa403739fec47c94aa7135a3a216e
Ruzeb <ruzeb1996@gmail.com>
2014-01-24 03:48:12 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,4 +1,11 @@
+import javax.swing.*;
+
+import java.awt.*;
+
 public class Driver {
+	
+	
+
 	public static void main (String[] args) {
 		Continent NorthAmerica = new Continent(528700000, 0.9); 
 		Continent SouthAmerica = new Continent(387500000, 0.2);
@@ -7,6 +14,18 @@ public class Driver {
 		Continent Asia = new Continent(2147482999,0.6); 
 		Continent Australia = new Continent(22680000,0.8); 
 		
-		
+		GOOEY gui = new GOOEY();
+		JButton b = new JButton();
+		JButton c = new JButton();
+		JButton d = new JButton();
+		JButton e = new JButton();
+		JButton f = new JButton();
+		JButton g = new JButton();
+		JButton h = new JButton();
+		JButton i = new JButton();
+		Upgrades u = new Upgrades(b,c,d,e,f,g,h,i);
+		u.buttoner(b);
+		u.buttoner(d);
+		u.buttoner(c);
 	}
 }
