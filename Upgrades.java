050a4c3fb7486cb01a42d33358ee5a0f283d4491
Ruzeb <ruzeb1996@gmail.com>
2014-01-24 21:48:18 -0500

    BIG UPDATE WITH TABBED PANES OMGGGG

--- Upgrades.java
+++ Upgrades.java
@@ -1,19 +1,18 @@
+
 import javax.swing.*;
 import javax.swing.border.Border;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
  
-public class Upgrades implements ActionListener{
+public class Upgrades extends JPanel implements ActionListener{
 	
-	//JButton x = new JButton();
-
     public Upgrades(JButton a, JButton b, JButton c, 
     		JButton d, JButton e, JButton f, JButton g, JButton h, JButton i) {
-    	
-    	JPanel mainPanel = new JPanel();
-    	
+    	super(new GridLayout(100,100));
+
     	// ================ SETTING ACTION LISTENERS ===============
     	a.setText("Air");
     	a.addActionListener(new ActionListener() {
@@ -95,11 +94,45 @@ public class Upgrades implements ActionListener{
                 //Execute when button is pressed
                 System.out.println("You clicked the button");
             }
-        //===========================================================    
-            
-            
     	});
-    	//mainPanel.add(title);
+        //===========================================================       
+    	//JPanel mainPanel = new JPanel();
+    	//mainPanel.setBorder(BorderFactory.createEmptyBorder(100, 100, 300, 100)); 
+    	JTabbedPane tabbedPane = new JTabbedPane();
+    	
+
+    	JComponent panel1 = makeTextPanel("Panel #1");
+    	tabbedPane.addTab("Transmission", null, panel1,
+    	                  "Does nothing");
+    	//tabbedPane.setTabPlacement(JTabbedPane.CENTER);
+    	tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+    	panel1.setPreferredSize(new Dimension(410, 100));
+    	panel1.add(a);
+    	panel1.add(b);
+    	panel1.add(c);
+    	
+    	
+    	JComponent panel2 = makeTextPanel("Panel #2");
+    	tabbedPane.addTab("Symptoms", null, panel2,
+    	                  "Does twice as much nothing");
+    	tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+    	panel2.setPreferredSize(new Dimension(410, 100));
+    	panel2.add(d);
+    	panel2.add(e);
+    	panel2.add(g);
+    	panel2.add(h);
+
+    	JComponent panel3 = makeTextPanel("Panel #3");
+    	tabbedPane.addTab("Fight the Cure", null, panel3,
+    	                  "Still does nothing");
+    	tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+    	panel3.add(h);
+    	panel3.add(i);
+    	
+    	
+    	add(tabbedPane);
+    	
+    	/*
     	mainPanel.add(a);
     	mainPanel.add(b);
     	mainPanel.add(c);
@@ -109,14 +142,12 @@ public class Upgrades implements ActionListener{
     	mainPanel.add(g);
     	mainPanel.add(h);
     	mainPanel.add(i);
-    	mainPanel.setBorder(BorderFactory.createEmptyBorder(100, 100, 600, 100));  
-    	
+    	*/
     	
         JFrame f1 = new JFrame("Upgrades");
-        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
        
-        
-        f1.setContentPane(mainPanel);
+        f1.setContentPane(tabbedPane);
         f1.pack();
         f1.setVisible(true);
         
@@ -134,27 +165,28 @@ public class Upgrades implements ActionListener{
 	 public void buttoner(JButton b){
 		 	
 	    	ImageIcon red = new ImageIcon("red.png");
-	    	ImageIcon gray = new ImageIcon("gray.png");
-	    
-	        
+	    	ImageIcon gray = new ImageIcon("gray.png");  
 	        b.setIcon(red);
-	        b.setRolloverIcon(gray);
-	        
-	        
+	        b.setRolloverIcon(gray); 
 	        b.setHorizontalTextPosition(JButton.CENTER);
 	        b.setRolloverEnabled(true);
 	        b.setFocusPainted(false);
 	        b.setBorderPainted(false);
 	        b.setContentAreaFilled(false);
 	    }
-
-
-
-
+	 protected JComponent makeTextPanel(String text) {
+	        JPanel panel = new JPanel(false);
+	        JLabel filler = new JLabel(text);
+	        filler.setHorizontalAlignment(JLabel.CENTER);
+	        panel.setLayout(new GridLayout(1, 1));
+	        panel.add(filler);
+	        return panel;
+	    }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
-    }
+}
+
+
