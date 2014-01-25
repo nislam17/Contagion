8f7703b21f2e097f241a8d024e5fa0949c8ef7e6
Ruzeb <ruzeb1996@gmail.com>
2014-01-24 20:18:10 -0500

    Update Upgrades.java with all buttons implemented with ActionListener

--- Upgrades.java
+++ Upgrades.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
  
@@ -9,8 +11,18 @@ public class Upgrades implements ActionListener{
 
     public Upgrades(JButton a, JButton b, JButton c, 
     		JButton d, JButton e, JButton f, JButton g, JButton h, JButton i) {
-    	JPanel p = new JPanel();
     	
+    	//Border subject = BorderFactory.createEmptyBorder();
+    	/*Border lineBorder = 
+    			BorderFactory.createTitledBorder
+    			(subject, "Upgrades", 50, 50, 
+    					new Font("Courier New",Font.ITALIC,12), Color.BLACK);
+    	JPanel title = new JPanel();
+    	title.setBorder(lineBorder);
+    	*/
+    	JPanel mainPanel = new JPanel();
+    	
+    	a.setText("Air");
     	a.addActionListener(new ActionListener() {
     		 
             public void actionPerformed(ActionEvent e)
@@ -19,25 +31,96 @@ public class Upgrades implements ActionListener{
                 System.out.println("You clicked the button");
             }
     	});
-    	
-    	
-    	
-    	
-    	p.add(a);
-    	p.add(b);
-    	p.add(c);
-    	p.add(d);
-    	p.add(e);
-    	p.add(f);
-    	p.add(g);
-    	p.add(h);
-    	 p.setBorder(BorderFactory.createEmptyBorder(100, 100, 700, 100));  
+    	b.setText("Water");
+    	b.addActionListener(new ActionListener() {
+    		 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                System.out.println("You clicked the button");
+            }
+    	});
+    	c.setText("Livestock");
+    	c.addActionListener(new ActionListener() {
+    		 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                System.out.println("You clicked the button");
+            }
+    	});
+    	d.setText("Insomnia");
+    	d.addActionListener(new ActionListener() {
+    		 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                System.out.println("You clicked the button");
+            }
+    	});
+    	e.setText("Paranoia");
+    	e.addActionListener(new ActionListener() {
+    		 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                System.out.println("You clicked the button");
+            }
+    	});
+    	f.setText("Paralysis");
+    	f.addActionListener(new ActionListener() {
+    		 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                System.out.println("You clicked the button");
+            }
+    	});
+    	g.setText("Coma");
+    	g.addActionListener(new ActionListener() {
+    		 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                System.out.println("You clicked the button");
+            }
+    	});
+    	h.setText("Genetic Reshuffle");
+    	h.addActionListener(new ActionListener() {
+    		 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                System.out.println("You clicked the button");
+            }
+    	});
+    	i.setText("Drug Resistance");
+    	i.addActionListener(new ActionListener() {
+    		 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                System.out.println("You clicked the button");
+            }
+    	});
+    	//mainPanel.add(title);
+    	mainPanel.add(a);
+    	mainPanel.add(b);
+    	mainPanel.add(c);
+    	mainPanel.add(d);
+    	mainPanel.add(e);
+    	mainPanel.add(f);
+    	mainPanel.add(g);
+    	mainPanel.add(h);
+    	mainPanel.add(i);
+    	mainPanel.setBorder(BorderFactory.createEmptyBorder(100, 100, 600, 100));  
     	
     	
         JFrame f1 = new JFrame("Upgrades");
         f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        
-        f1.setContentPane(p);
+        
+        f1.setContentPane(mainPanel);
         f1.pack();
         f1.setVisible(true);
         
@@ -49,13 +132,9 @@ public class Upgrades implements ActionListener{
         buttoner(f);
         buttoner(g);
         buttoner(h);
+        buttoner(i);
     }
     
-    
-   
-    
-    
-
 	 public void buttoner(JButton b){
 		 	
 	    	ImageIcon red = new ImageIcon("red.png");
@@ -83,4 +162,3 @@ public class Upgrades implements ActionListener{
 		
 	}
     }
-
