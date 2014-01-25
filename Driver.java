c81cb19890945385b9cb88e75c58a6ee26c9d25c
Ruzeb <ruzeb1996@gmail.com>
2014-01-25 18:39:25 -0500

    restored

--- Driver.java
+++ Driver.java
@@ -4,11 +4,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Driver extends JPanel implements ActionListener{
-	
-	private static boolean winner; 
-	private static String game = "continue";
         
-	final static Continent NorthAmerica = new Continent(528700000, 0.9, "NA"); 
+        private static boolean winner; 
+        private static String game = "continue";
+        
+        final static Continent NorthAmerica = new Continent(528700000, 0.9, "NA"); 
     final static Continent SouthAmerica = new Continent(387500000, 0.2, "SA");
     final static Continent Europe = new Continent(739200000, 0.4, "EU"); 
     final static Continent Africa = new Continent(1033000000,0.1, "AF"); 
@@ -17,20 +17,74 @@ public class Driver extends JPanel implements ActionListener{
     
     static Continent [] ContinentArray = {NorthAmerica, SouthAmerica, Europe, Africa, Asia, Australia};
     
-	static Pathogen pathogen;
-		
-		public static void Continental(){
-			final JPanel ConChooser = new JPanel(); //choose continent
-			final JFrame f2 = new JFrame("Choose your Continent");
-			JButton NA = new JButton("North America");
+        static Pathogen pathogen;
+        
+        //game play 
+    public static void gamePlay() {
+    
+        while (game.equals("continue")) {
+                System.out.println("ahhh");
+                for (Continent c: ContinentArray) {
+                        pathogen.infect(c);        //infects population of invaded continents
+                
+                }
+                pathogen.transmit(ContinentArray[(int)(Math.random() * 6)] ); //transmits to a random continent
+                pathogen.resist(NorthAmerica);  //resists cure (doesn't matter of which continent because cure is static)
+                for (Continent c: ContinentArray) {
+                        pathogen.kill(c);  //kills population of infected continents
+                }
+                for (Continent c: ContinentArray) {
+                        if (c.getDeadContinent() == true ); // if the continent is dead, it cannot contribute to the cure 
+                        else {
+                                c.produceCure(); // every infected (& alive) country produces a cure 
+                        }
+                        
+                }
+                for (Continent c: ContinentArray) {
+                        c.Cure(); // cures every continent that needs it 
+                }
+                for (Continent c: ContinentArray) {
+                        int counterDead = 0; //counter for the # of dead continents 
+                        if (c.getDeadContinent() == true) { // checks if continent is dead
+                                counterDead += 1; 
+                        }
+                        if (counterDead == 6) { // if all 6 continents are dead, the player is a winner 
+                                winner = true; 
+                                game = "stop";
+                                break;
+                        }
+                        int counterHealthy = 0; //counter for the # of healthy continents
+                        if (c.getInvaded() == false) { //checks if continent is healthy
+                                counterHealthy += 1; 
+                        }
+                        if (counterHealthy == 6) { // if all 6 continents are healthy, the player is a loser
+                                winner = false;
+                                game = "stop";
+                                break;
+                        }
+                }
+        }
+        if (winner) {
+                System.out.println("CONGRATS YOU WON!");
+        }
+        else {
+                System.out.println("Good luck next time buddy. You lost.");
+        }
+        
+    }
+                
+                public static void Continental(){
+                        final JPanel ConChooser = new JPanel(); //choose continent
+                        final JFrame f2 = new JFrame("Choose your Continent");
+                        JButton NA = new JButton("North America");
             NA.addActionListener(new ActionListener() {
                     
             public void actionPerformed(ActionEvent cr)
             {
                 //Execute when button is pressed
                     pathogen.transmit(NorthAmerica);
-                    System.out.println(NorthAmerica.getInvaded());
-                    f2.dispose();      
+                    f2.dispose();   
+                    gamePlay();
             }
             });
                 
@@ -41,8 +95,8 @@ public class Driver extends JPanel implements ActionListener{
             {
                 //Execute when button is pressed
                     pathogen.transmit(SouthAmerica);
-                    System.out.println(SouthAmerica.getInvaded());
-                    f2.dispose();    
+                    f2.dispose();  
+                    gamePlay();
             }
             });
                 
@@ -51,9 +105,9 @@ public class Driver extends JPanel implements ActionListener{
                     
             public void actionPerformed(ActionEvent cr)
             {
-            	pathogen.transmit(Europe);
-                System.out.println(Europe.getInvaded());
+                    pathogen.transmit(Europe);
                     f2.dispose(); 
+                    gamePlay();
             }
             });
                 
@@ -62,9 +116,9 @@ public class Driver extends JPanel implements ActionListener{
                     
             public void actionPerformed(ActionEvent cr)
             {
-            	pathogen.transmit(Africa);
-                System.out.println(Africa.getInvaded());
+                    pathogen.transmit(Africa);
                     f2.dispose();
+                    gamePlay();
             }
             });
                 
@@ -73,9 +127,9 @@ public class Driver extends JPanel implements ActionListener{
                     
             public void actionPerformed(ActionEvent cr)
             {
-            	pathogen.transmit(Asia);
-                System.out.println(Asia.getInvaded());
+                    pathogen.transmit(Asia);
                     f2.dispose();
+                    gamePlay();
             }
             });
                 
@@ -84,9 +138,9 @@ public class Driver extends JPanel implements ActionListener{
                     
             public void actionPerformed(ActionEvent cr)
             {
-            	pathogen.transmit(Australia);
-                System.out.println(Australia.getInvaded());
+                    pathogen.transmit(Australia);
                     f2.dispose();
+                    gamePlay();
             }
             });
                 
@@ -101,63 +155,12 @@ public class Driver extends JPanel implements ActionListener{
                 f2.setContentPane(ConChooser);
                 f2.pack();
                 f2.setVisible(true);  
-		}
-		
-		public static void gamer(){
-			System.out.println(NorthAmerica.getInfected());
-			while (game.equals("continue")) {
-	        	for (Continent c: ContinentArray) {
-	        		pathogen.infect(c);	//infects population of invaded continents
-	        	}
-	        	pathogen.transmit(ContinentArray[(int)(Math.random() * 6)] ); //transmits to a random continent
-	        	pathogen.resist(NorthAmerica);  //resists cure (doesn't matter of which continent because cure is static)
-	        	for (Continent c: ContinentArray) {
-	        		pathogen.kill(c);  //kills population of infected continents
-	        	}
-	        	for (Continent c: ContinentArray) {
-	        		if (c.getDeadContinent() == true ); // if the continent is dead, it cannot contribute to the cure 
-	        		else {
-	        			c.produceCure(); // every infected (& alive) country produces a cure 
-	        		}
-	        		
-	        	}
-	        	for (Continent c: ContinentArray) {
-	        		c.Cure(); // cures every continent that needs it 
-	        	}
-	        	for (Continent c: ContinentArray) {
-	        		int counterDead = 0; //counter for the # of dead continents 
-	        		if (c.getDeadContinent() == true) { // checks if continent is dead
-	        			counterDead += 1; 
-	        		}
-	        		if (counterDead == 6) { // if all 6 continents are dead, the player is a winner 
-	        			winner = true; 
-	        			game = "stop";
-	        			return;
-	        		}
-	        		int counterHealthy = 0; //counter for the # of healthy continents
-	        		if (c.getInvaded() == false) { //checks if continent is healthy
-	        			counterHealthy += 1; 
-	        		}
-	        		if (counterHealthy == 6) { // if all 6 continents are healthy, the player is a loser
-	        			winner = false;
-	        			game = "stop";
-	        			return;
-	        		}
-	        	}
-	        }
-	        if (winner) {
-	        	System.out.println("CONGRATS YOU WON!");
-	        }
-	        else {
-	        	System.out.println("Good luck next time buddy. You lost.");
-	        }
-	    }
-		
-		
+                }
+                
         public static void main (String[] args) {
 
-        	final JFrame f1 = new JFrame("Choose Your Pathogen");
-        	
+                final JFrame f1 = new JFrame("Choose Your Pathogen");
+
                 
                 JPanel chooser = new JPanel(); // choose pathogen
                 //chooser.setSize(new Dimension(500,500));
@@ -168,12 +171,10 @@ public class Driver extends JPanel implements ActionListener{
             {
                 //Execute when button is pressed
                     pathogen = new Bacteria(); 
-                System.out.println("You clicked the button");
                 GOOEY gui = new GOOEY();
                 f1.dispose();
                 Continental();
-                gamer();
-                
+                 
             }
             });
                 JButton Virus = new JButton("Virus");
@@ -183,12 +184,10 @@ public class Driver extends JPanel implements ActionListener{
             {
                 //Execute when button is pressed
                     pathogen = new Virus();
-                System.out.println("You clicked the button");
+
                 GOOEY gui = new GOOEY();
                 f1.dispose();
-                Continental();  
-                gamer();
-                
+                Continental();     
             }
             });
                 JButton Senioritis = new JButton("Senioritis");
@@ -198,12 +197,9 @@ public class Driver extends JPanel implements ActionListener{
             {
                 //Execute when button is pressed
                     pathogen = new Senioritis();
-                System.out.println("You clicked the button");
                 GOOEY gui = new GOOEY();
                 f1.dispose();
                 Continental(); 
-                gamer();
-                
             }
             });
                 chooser.add(Bacteria);
@@ -211,16 +207,20 @@ public class Driver extends JPanel implements ActionListener{
                 chooser.add(Senioritis);
                 
                 f1.setLocation(500, 500);
-		        f1.setContentPane(chooser);
-		        f1.pack();
-		        f1.setVisible(true);
-		        
-        }
+                        f1.setContentPane(chooser);
+                        f1.pack();
+                        f1.setVisible(true);
+                        
+                      
+                       
+        }// end main 
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+                // TODO Auto-generated method stub
+                
+        
+        
+        
 }
+}}
